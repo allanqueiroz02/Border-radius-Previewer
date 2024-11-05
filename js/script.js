b01e0a733449d4b5777f8674f0eaf12ffa1c90fd
Allan Dawid <allan.queiroz.dev@gmail.com>
2024-11-04 21:11:55 -0300

    adicionada fn de copiar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log("Carregou script");
+const textSheet = document.querySelector(".borders");
+const copyToClip = document.querySelector(".copy-clipboard");
+if (copyToClip)
+    copyToClip.addEventListener("click", (event) => {
+        const copyText = textSheet?.textContent || "";
+        navigator.clipboard.writeText(copyText);
+        alert("Copiou legal dog");
+    });
--- script.ts
+++ script.ts
@@ -1,1 +1,11 @@
 console.log("Carregou script");
+
+const textSheet = document.querySelector(".borders");
+const copyToClip = document.querySelector(".copy-clipboard");
+
+if (copyToClip)
+  copyToClip.addEventListener("click", (event) => {
+    const copyText = textSheet?.textContent || "";
+    navigator.clipboard.writeText(copyText);
+    alert("Copiou legal dog");
+  });
