895c59bedc76adc1d5575e34b52eab327811bfc2
Allan Dawid <allan.queiroz.dev@gmail.com>
2024-11-12 22:51:34 -0300

    ajustadas funcoes

--- js/script.js
+++ js/script.js
@@ -2,17 +2,21 @@ const textSheet = document.querySelector(".my-square");
 const inputBorderRadius = document.querySelector(".input-border");
 const copyToClip = document.querySelector(".copy-clipboard");
 const toastr = document.querySelector(".warning-copied");
-export const formatBorderRadiusText = (value) => `border-radius: ${value}`;
-inputBorderRadius.addEventListener("keyup", () => {
+export const formatBorderRadiusText = (value) => `border-radius: ${value};`;
+const handleKeyUpInput = () => {
     textSheet.style.borderRadius = inputBorderRadius.value;
     textSheet.style.transition = "border-radius 0.7s";
     textSheet.innerText = formatBorderRadiusText(inputBorderRadius.value);
-});
-copyToClip?.addEventListener("click", () => {
+};
+const handleClickCopyBtn = () => {
     const copyText = formatBorderRadiusText(inputBorderRadius.value) || "";
     navigator.clipboard.writeText(copyText);
     toastr?.classList.add("show");
     setTimeout(() => {
         toastr?.classList.remove("show");
     }, 2500);
-});
+};
+if (inputBorderRadius)
+    inputBorderRadius.addEventListener("keyup", handleKeyUpInput);
+if (copyToClip)
+    copyToClip?.addEventListener("click", handleClickCopyBtn);
--- script.ts
+++ script.ts
@@ -5,19 +5,24 @@ const inputBorderRadius = document.querySelector(
 const copyToClip = document.querySelector(".copy-clipboard");
 const toastr = document.querySelector(".warning-copied");
 
-export const formatBorderRadiusText = (value: string) => `border-radius: ${value}`;
+export const formatBorderRadiusText = (value: string) =>
+  `border-radius: ${value};`;
 
-inputBorderRadius.addEventListener("keyup", () => {
+const handleKeyUpInput = () => {
   textSheet.style.borderRadius = inputBorderRadius.value;
   textSheet.style.transition = "border-radius 0.7s";
   textSheet.innerText = formatBorderRadiusText(inputBorderRadius.value);
-});
+};
 
-copyToClip?.addEventListener("click", () => {
+const handleClickCopyBtn = () => {
   const copyText = formatBorderRadiusText(inputBorderRadius.value) || "";
   navigator.clipboard.writeText(copyText);
   toastr?.classList.add("show");
   setTimeout(() => {
     toastr?.classList.remove("show");
   }, 2500);
-});
+};
+
+if (inputBorderRadius)
+  inputBorderRadius.addEventListener("keyup", handleKeyUpInput);
+if (copyToClip) copyToClip?.addEventListener("click", handleClickCopyBtn);
